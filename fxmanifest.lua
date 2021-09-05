d9b1e26ebab8c57e2bec2e83154e12b108ee1338
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-09-05 02:23:09 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,4 @@
-fx_version 'adamant'
-
+fx_version 'cerulean'
 game 'gta5'
 
 description 'ESX Vangelico Robbery'
@@ -8,25 +7,17 @@ version '2.0.0'
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
+	'locales/*.lua',
 	'config.lua',
 	'client/esx_vangelico_robbery_cl.lua'
 }
 
 server_scripts {
-    '@mysql-async/lib/MySQL.lua',
+        '@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/de.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
+	'locales/*.lua',
 	'config.lua',
 	'server/esx_vangelico_robbery_sv.lua'
 }
 
-dependencies {
-	'es_extended'
-}
+dependency 'es_extended'
