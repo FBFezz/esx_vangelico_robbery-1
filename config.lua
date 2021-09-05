35621b05c303faa370407a52332c9f539ccf1be2
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-09-05 10:39:14 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -12,8 +12,6 @@ Config.PriceForOneJewel = 500
 Config.EnableMarker = true
 Config.NeedBag = true
 
-Config.Borsoni = {40, 41, 44, 45}
-
 Stores = {
 	["jewelry"] = {
 		position = { ['x'] = -629.99, ['y'] = -236.542, ['z'] = 38.05 },       
